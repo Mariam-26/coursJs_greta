b6e59b17dd8293dc3a4e2ecca25d4a44c7eb19ca
Mariam <82433410+Mariam-26@users.noreply.github.com>
2022-11-24 18:48:08 +0100

    fichier externe en javaScript

---
+++ fonctions.js
@@ -0,0 +1,9 @@
+/**
+ * funtion nom(parametre1, parametre2)
+ * {
+ * code
+ * }
+ * PR EXECUTER LA FONCTION "nom()"
+ * 
+ * nom(valeur1, valeur2)
+ */
---
+++ js.js
@@ -0,0 +1,25 @@
+/**
+ * var = declarer, re declarer et modifier sa valeur
+ * let = declarer,ne pas redeclarer et modifier sa valeur
+ * const = declarer,ne pas redeclarer et ne pas modifier sa valeur
+ */
+
+// je declare lae variables
+var string = "Bonjour"; // valeur et type string (chaine de caractaire)
+var num = 15; // valeur et type int (numerique)
+var numString = "15"; // valeur string et type int
+
+var vrais=true;
+var faux=false;
+
+var tableau=[];
+
+var vide="";
+var nonVide=" ";
+
+
+
+// je declare la fonction maFunction()
+function maFunction() {
+    alert("Hello");
+}
