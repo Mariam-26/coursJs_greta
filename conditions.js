17976235acbee23d5e7621b93f085d1cd6013b3e
Mariam <82433410+Mariam-26@users.noreply.github.com>
2022-11-24 14:02:43 +0100

    conditions en javaScript

---
+++ conditions.js
@@ -0,0 +1,66 @@
+/*
+IF
+SWITCH
+TERNAIRE
+TRY CATCH
+ */
+
+/**
+ * if(condition)
+ * {
+ * code
+ * }
+ */
+
+var a=10;
+var b="10";
+var d= 30;
+var c=20;
+
+if (a>b) {
+  alert("OK");
+}
+if (a<b) {
+  alert("OK");
+}
+if (a>=b) {
+  alert("OK");
+}
+if (a<=b) {
+  alert("OK");
+}
+if (a!=b) {
+  alert("OK");
+}
+if (a!==b) {
+  alert("OK");
+}
+if (a>b) {
+  alert("OK");
+}
+
+if(a===b) {
+  alert("OK");
+}else{
+  alert("KO")
+}
+
+if(a==b) {
+  alert("OK");
+}else if (c==d) {
+  alert("OK");
+}else{
+  alert("KO")
+}
+
+// CONDITION COMBINEE
+if (a==b && c==d) {
+  alert("KO");
+}
+
+if(a==b || c==d) {
+  alert("KO");
+}
+if((a==b || c==d) && a==c) {
+  alert("KO");
+}
