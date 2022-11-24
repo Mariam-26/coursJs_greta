a1cc89d581d1e8967d206907207d43d9f89fe8d5
Mariam <82433410+Mariam-26@users.noreply.github.com>
2022-11-24 19:06:11 +0100

    modification de addition.html

--- exos/addition.js
+++ exos/addition.js
@@ -1,26 +1,4 @@
-// DECLARATION DE LA FONCTION addition() SANS PARAMETRE
-function addition(){
-  var a = 2;
-  var b = 4;
-  var c = a+b
-  alert(c);
-}
-// DECLARATION DE LA FONCTION addition2() AVEC PARAMETRE
-// function addition2(a,b){
-//   alert(a+b);
-// }
-
-// addition2(10,20);
-// addition2(30,40);
-
-// function addition2(a,b,C=""){
-//   alert(a+b);
-// }
-
-// addition2(10,20,30);
-// addition2(30,40);
-
-function addition3(a, b){
+function addition1(a, b){
   var message = "";
   var c = a+b;
   if(c==30) {
